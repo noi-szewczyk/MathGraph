0391aa13259ab658cd2f31d6099bfc7a484bad25
Kucia <138583511+noi-szewczyk@users.noreply.github.com>
2023-12-30 22:47:05 +0100

    add copyright

--- player.py
+++ player.py
@@ -1,5 +1,20 @@
+"""
+Copyright© 2024 Artur Pozniak <noi.kucia@gmail.com> or <noiszewczyk@gmail.com>.
+All rights reserved.
+This program is released under license GPL-3.0-or-later
+
+This file is part of MathGraph.
+MathGraph is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+MathGraph is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with MathGraph.
+If not, see <https://www.gnu.org/licenses/>.
+"""
 import random
-import time
 
 import arcade
 
